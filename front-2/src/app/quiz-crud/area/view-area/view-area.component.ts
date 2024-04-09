3d03940f2aac511fb8c27a591a62f140db21fd1f
Milica <milicamikanovic1@gmail.com>
2024-04-09 22:48:00 +0100

    view area

--- front-2/src/app/quiz-crud/area/view-area/view-area.component.ts
+++ front-2/src/app/quiz-crud/area/view-area/view-area.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-view-area',
   templateUrl: './view-area.component.html',
-  styleUrls: ['./view-area.component.css']
+  styleUrls: ['./view-area.component.css'],
 })
 export class ViewAreaComponent {
+  showTooltipFlag: boolean = false;
+  
+  showTooltip() {
+    this.showTooltipFlag = true;
+  }
 
+  hideTooltip() {
+    this.showTooltipFlag = false;
+  }
 }
